b428c08d7ce901d376fec26605c8542a3d958ef6
MikeWilkinson <MikeWilkinson@10.0.1.2>
2017-07-26 23:08:41 -0400

    Controller and TimeKeeper Stubs

--- WheelOfJeopardy/src/wheelOfJeopardy/Controller.java
+++ WheelOfJeopardy/src/wheelOfJeopardy/Controller.java
@@ -1,5 +1,40 @@
 package wheelOfJeopardy;
 
 public class Controller {
+	private QuestionBoard QuestionBoard;
+	private Player[] Players;
+	private Player CurrentPlayer;
+	private ScoreBoard ScoreBoard;
+	private TimeKeeper TimeKeeper;
+	private Wheel Wheel;
+	//need to add View as an attribute
+	
+	public void startGame(){
+		
+	}
+	public void stopGame(){
+		
+	}
+	public void spin(){
+		
+	}
+	public void loseATurn(){
+		
+	}
+	public Question getQuestionForCategory(String theCategory){
+		return this.QuestionBoard.getQuestionForCategory(theCategory);
+	}
+	public void getPlayersChoice(){
+		
+	}
+	public void getOpponentsChoiceFrom(Player theOpponent){
+		
+	}
+	public Player getCurrentPlayer(){
+		return this.CurrentPlayer;
+	}
+	public void useTokenForCurrentPlayer(){
+		
+	}
 
 }
--- WheelOfJeopardy/src/wheelOfJeopardy/TimeKeeper.java
+++ WheelOfJeopardy/src/wheelOfJeopardy/TimeKeeper.java
@@ -2,4 +2,23 @@ package wheelOfJeopardy;
 
 public class TimeKeeper {
 
+	public boolean TimeIsUp;
+	private int TimeDuration;
+	
+	public TimeKeeper(){
+		this.TimeIsUp = false;
+	}
+	public void startTimer(int theDuration){
+		this.TimeDuration = theDuration;
+	}
+	public void stopTimer(){
+		
+	}
+	public void pauseTimer(){
+		
+	}
+	public void continueTimer(){
+		
+	}
+	
 }
