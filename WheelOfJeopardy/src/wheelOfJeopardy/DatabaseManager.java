4cdff291459c69506541cf64e7f8e688bc033fe2
ben <bab2155@gmail.com>
2017-08-15 20:26:12 -0400

    Cleaned up commented code

--- WheelOfJeopardy/src/wheelOfJeopardy/DatabaseManager.java
+++ WheelOfJeopardy/src/wheelOfJeopardy/DatabaseManager.java
@@ -173,17 +173,6 @@ public class DatabaseManager {
 
 		QuestionBoard[] gameQuestionBoards = tmp.toArray(new QuestionBoard[2]);
 
-
-//		HashSet<String> categorySet = new HashSet<String>();
-//
-//		for(int i=1; i < this.gameQuestions.length; i++){
-//			categorySet.add(this.gameQuestions[i].getCategory());
-//		}
-
-//		String[] categories = categorySet.toArray(new String[categorySet.size()]);
-//		QuestionBoard questionBoard = new QuestionBoard(roundCategories, this.gameQuestions);
-
-//		return questionBoard;
 		return gameQuestionBoards;
 
 	}
